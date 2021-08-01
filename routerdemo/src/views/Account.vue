1de820002a80a43e9a7b7bc234017ce149673f04
amitgarge <38751675+amitgarge@users.noreply.github.com>
2021-08-01 16:25:01 +0530

    initial commit

---
+++ routerdemo/src/views/Account.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>My Account Page</h1>
+  <h2>{{ $route.params }}</h2>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style>
+</style>
---
+++ tictactoe/src/App.vue
@@ -0,0 +1,165 @@
+<template>
+  <main
+    class="d-flex flex-column justify-content-center align-items-center bg-dark"
+  >
+    <div class="container">
+      <div class="row justify-content-center mt-3">
+        <div class="col col-6">
+          <div class="text-center">
+            <div v-if="!winMessage">
+              <h1 class="text-info" v-show="isCross">Cross Turn</h1>
+              <h1 class="text-info" v-show="!isCross">Circle Turn</h1>
+            </div>
+            <div v-else>
+              <h1 class="text-warning">
+                {{ winMessage.toUpperCase() }}
+              </h1>
+            </div>
+          </div>
+          <div class="grid">
+            <div
+              v-for="(item, i) in itemArray"
+              :key="i"
+              @click="handleClick(i)"
+              class="
+                card card-body
+                box
+                justify-content-center
+                align-items-center
+                bg-light
+              "
+            >
+              <icon :iconname="item"></icon>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center mt-3">
+        <button class="btn btn-danger btn-block pl-5 pr-5" @click="reloadGame">
+          Reset the Game
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import icon from "./components/icon.vue";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+
+export default {
+  name: "App",
+  components: {
+    icon,
+  },
+  data() {
+    return {
+      winMessage: "",
+      isCross: true,
+      itemArray: new Array(9).fill("empty"),
+    };
+  },
+  watch: {
+    winMessage: function (message) {
+      if (message) {
+        this.showDialog();
+      }
+    },
+  },
+  methods: {
+    showDialog() {
+      Swal.fire({
+        icon: "info",
+        title: "Game Over",
+        text: `${this.winMessage}`,
+      });
+    },
+    isArrayFull() {
+      const isbelow = (itemNumber) => itemNumber !== "empty";
+      return this.itemArray.every(isbelow);
+    },
+    handleClick(itemNumber) {
+      if (this.winMessage) return this.showDialog();
+
+      if (this.itemArray[itemNumber] === "empty") {
+        this.itemArray[itemNumber] = this.isCross ? "cross" : "circle";
+        this.isCross = !this.isCross;
+        this.checkIsWinner();
+      } else if (!this.isArrayFull()) return Swal.fire("already filled");
+      else this.winMessage = "It's a draw";
+    },
+    checkIsWinner() {
+      //  checking  winner of the game
+      if (
+        this.itemArray[0] === this.itemArray[1] &&
+        this.itemArray[0] === this.itemArray[2] &&
+        this.itemArray[0] !== "empty"
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[3] !== "empty" &&
+        this.itemArray[3] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[5]
+      ) {
+        this.winMessage = `${this.itemArray[3]} won`;
+      } else if (
+        this.itemArray[6] !== "empty" &&
+        this.itemArray[6] === this.itemArray[7] &&
+        this.itemArray[7] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[6]} won`;
+      } else if (
+        this.itemArray[0] !== "empty" &&
+        this.itemArray[0] === this.itemArray[3] &&
+        this.itemArray[3] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[1] !== "empty" &&
+        this.itemArray[1] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[7]
+      ) {
+        this.winMessage = `${this.itemArray[1]} won`;
+      } else if (
+        this.itemArray[2] !== "empty" &&
+        this.itemArray[2] === this.itemArray[5] &&
+        this.itemArray[5] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      } else if (
+        this.itemArray[0] !== "empty" &&
+        this.itemArray[0] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[2] !== "empty" &&
+        this.itemArray[2] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      } else this.isArrayFull();
+      this.winMessage = "It's Draw";
+    },
+    reloadGame() {
+      this.winMessage = "";
+      (this.isCross = true), (this.itemArray = new Array(9).fill("empty"));
+    },
+  },
+};
+</script>
+
+<style>
+main {
+  height: 100vh;
+}
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5px;
+}
+
+.box {
+  height: 150px;
+}
+</style>
